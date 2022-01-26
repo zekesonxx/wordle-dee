4bcf7179c4bf8f3ebcc1dc11cf9b84151900e54d
Zoey Mertes <zmertes@purdue.edu>
2022-01-26 17:24:39 -0500

    Refactor into LetterStats structs

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,46 @@ extern crate rayon;
 use std::collections::HashMap;
 use rayon::prelude::*;
 
+#[derive(Clone, Copy, Default, Debug)]
+struct LetterStats {
+    pub global_count: usize,
+    pub global_freq: f64,
+    pub local_count: [usize; 5],
+    pub local_freq: [f64; 5]
+}
+
+impl LetterStats {
+    fn new() -> Self {
+        LetterStats::default()
+    }
+    fn calculate_freqs(&mut self, global_total: usize) {
+        self.global_freq = self.global_count as f64 / global_total as f64;
+        let local_total = (global_total/5) as f64;
+        self.local_freq = [self.local_count[0] as f64/local_total,
+                           self.local_count[1] as f64/local_total,
+                           self.local_count[2] as f64/local_total,
+                           self.local_count[3] as f64/local_total,
+                           self.local_count[4] as f64/local_total];
+    }
+}
+
 fn main() {
     let dict = include_str!("../dict.txt");
     let dict: Vec<&str> = dict.split('\n').collect();
     let answers = include_str!("../answers.txt");
     let answers: Vec<&str> = answers.split('\n').collect();
-    let mut global_letters = HashMap::<char, usize>::with_capacity(26);
+    let mut letters = HashMap::<char, LetterStats>::with_capacity(26);
     let mut global_total = 0usize;
-    let mut letters = [HashMap::<char, usize>::with_capacity(26), HashMap::<char, usize>::with_capacity(26), HashMap::<char, usize>::with_capacity(26), HashMap::<char, usize>::with_capacity(26), HashMap::<char, usize>::with_capacity(26)];
     for answer in answers {
         global_total += 5;
         for (pos, c) in answer.chars().enumerate() {
-            let gcounter = global_letters.entry(c).or_insert(0);
-            *gcounter += 1;
-            let lcounter = letters[pos].entry(c).or_insert(0);
-            *lcounter += 1;
+            let letter = letters.entry(c).or_insert_with(LetterStats::default);
+            letter.global_count += 1;
+            letter.local_count[pos] += 1;
         }
     }
-    let total = global_total/5;
-
-    let mut gl: Vec<(&char, &usize)> = global_letters.iter().collect();
-    gl.sort_unstable_by(|a, b| a.1.cmp(b.1));
-    gl.reverse();
-    println!("global letter percentages:");
-    for (c, count) in &gl {
-        println!(" {} {} {:.2}%", c, count, (**count as f64)/(global_total as f64)*100.0);
+    for letter in letters.values_mut() {
+        (*letter).calculate_freqs(global_total);
     }
-    for i in 0..5 {
-        println!("position {} percentages:", i+1);
-        let mut l: Vec<(&char, &usize)> = letters[i].iter().collect();
-        l.sort_unstable_by(|a, b| a.1.cmp(b.1));
-        l.reverse();
-        for (c, count) in &l {
-            println!(" {} {} {:.2}%", c, count, (**count as f64)/(total as f64)*100.0);
-        }
-    }
+    println!("{:?}", letters);
 }
